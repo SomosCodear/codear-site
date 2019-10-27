b323d6aedf8cfd7632f034eb98017ce3d700e8e0
Agustin Carrasco <asermax@gmail.com>
2019-10-27 15:59:50 -0300

    feat(index): add communities section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import events from '../events.json';
+import Head from 'next/head';
 import { Container } from '../components/Container';
 import { Nav } from '../components/Nav';
 import { Calendar } from '../components/Calendar';
@@ -11,23 +12,84 @@ const MainContainer = styled.main`
   flex-direction: column;
   align-items: stretch;
   overflow: auto;
+
+  h1 {
+    margin: 0;
+    font-size: 4.5rem;
+    font-weight: 100;
+    color: var(--color-primary-light);
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
+
+const CommunitiesSection = styled.section`
+  margin-top: 3rem;
+
+  h1 {
+    font-size: 3.25rem;
+  }
+`;
+
+const CommunitiesBanner = styled.div`
+  align-self: stretch;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 1.25rem;
+  padding: 1.875rem;
+  background-image: url(/images/backgrounds/community.png);
+  background-repeat: no-repeat;
+  background-position: left center;
+  background-size: cover;
+
+  img + img {
+    margin-left: 0.5rem;
+  }
+`
 
 const Index = () => {
   useEffect(() => {
     if(typeof window !== 'undefined') {
-      window.WOW = require('@codear/lilac');
+      require('@codear/lilac');
     }
   }, []);
 
   return (
     <Container>
+      <Head>
+        <title>CoDeAr</title>
+      </Head>
       <Nav />
       <MainContainer>
         <Calendar
           name="calendario"
           events={events}
         />
+        <CommunitiesSection>
+          <h1>
+            comunidades
+          </h1>
+          <CommunitiesBanner>
+            <img src="/images/community-logos/beer-js.png" alt="BeerJS" />
+            <img src="/images/community-logos/met-cba.png" alt="MeT Cba" />
+            <img src="/images/community-logos/facebook-dev-circle.png" alt="Facebook Dev Circle" />
+          </CommunitiesBanner>
+        </CommunitiesSection>
+        <section>
+          <h1>
+            proyectos
+          </h1>
+        </section>
+        <section>
+          <h1>
+            nosotros
+          </h1>
+        </section>
       </MainContainer>
     </Container>
   );
