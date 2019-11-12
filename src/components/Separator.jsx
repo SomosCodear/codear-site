ae341423bf2cbc16616afe69e5618db1ed8a7af5
Agustin Carrasco <asermax@gmail.com>
2019-11-12 09:56:36 -0300

    fix(separator): hide on mobile

--- src/components/Separator.jsx
+++ src/components/Separator.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: none;
+
   @media (min-width: 80rem) {
+    display: block;
     background: linear-gradient(90deg, #350C4B, #474A7D);
     min-height: 0.75rem;
     max-width: 100%;
     overflow-x: hidden;
+
     img {
       margin: 0 auto;
       display: block;
