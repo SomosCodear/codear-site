1aca297b5604dbf7c81d18840dfb5ec5c1b9cd23
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2020-03-28 15:07:09 -0300

    launched edupunto

--- src/pages/knowledge.jsx
+++ src/pages/knowledge.jsx
@@ -204,6 +204,34 @@ const Knowledge = () => {
     </Content>,
     <ProgramBanner>
       <Content>
+        <h2>edupunto.</h2>
+        <p>
+          Abrimos este espacio para proponer herramientas, plataformas
+          tecnológicas, conocimiento y experiencias que creemos que pueden sumar a la
+          transformación digital de la educación.
+        </p>
+        <lilac-button
+          href="https://edu.codear.org"
+          target="_blank"
+        >
+          Visitá edupunto
+        </lilac-button>
+      </Content>
+    </ProgramBanner>,
+    <Content>
+      <p>
+        Esta iniciativa surge a partir de las conversaciones que tuvieron lugar en el primer
+        #EncuentroCodear de 2020, donde nos reunimos con docentes y educadores de diversos
+        puntos del país para discutir dónde nos encontramos hoy en día y hacia dónde podemos ir.
+      </p>
+      <p>
+        Publicaremos contenido frecuentemente para acompañar este proceso repentino de
+        transformación de la educación, centralizando en este portal buenas prácticas,
+        ideas, herramientas y más.
+      </p>
+    </Content>,
+    <ProgramBanner>
+      <Content>
         <h2>#EncuentrosCodear</h2>
       </Content>
     </ProgramBanner>,
