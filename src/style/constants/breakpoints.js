2dfe9ada32599cc7ff99fd107aae16a305ee6852
alexrobaina <alexrobainaph@gmail.com>
2020-08-31 21:39:17 -0300

    delete comments

--- src/style/constants/breakpoints.js
+++ src/style/constants/breakpoints.js
@@ -7,12 +7,3 @@ export const BREAKPOINTS = {
   hd: '80rem',
   wideScreen: '120rem',
 };
-
-// 120rem equicale a 1920px
-// 80rem equicale a 1280px
-// 45rem equicale a 720px
-
-// entre 80 y 120 tal cosa
-
-// ${({ modoTarjeta }) => modoTarjta ? SectionTarjeta : SectionCalendar}
-// asermax@gmail.com
