0dd4289ebed7d85296653a4ab6aea43a09674a6f
alexrobaina <alexrobainaph@gmail.com>
2020-11-09 07:31:53 -0300

    resolve fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,6 +6,7 @@ import withGA from 'next-ga';
 import { ANALYTICS_ID } from '../data/config';
 import { Container } from '../components/Container';
 
+console.log('akjsfghasku.');
 /* eslint-disable react/prop-types, react/jsx-props-no-spreading */
 const CodearApp = ({ Component, pageProps }) => (
   <Container {...Component.layoutProps}>
