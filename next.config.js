da57eac4480265420a9470e6ba253b10e75ef05e
Agustin Carrasco <asermax@gmail.com>
2021-01-02 23:21:55 -0300

    chore(next): upgrade to 10.x

--- next.config.js
+++ next.config.js
@@ -2,19 +2,17 @@ module.exports = {
   env: {
     ANALYTICS_ID: process.env.ANALYTICS_ID,
   },
-  experimental: {
-    async rewrites() {
-      return [
-        { source: '/', destination: '/' },
-        { source: '/conocenos', destination: '/about' },
-        { source: '/codigo-de-conducta', destination: '/coc' },
-        { source: '/comunicaciones', destination: '/comuniques' },
-        { source: '/comunidades', destination: '/communities' },
-        { source: '/conocimiento', destination: '/knowledge' },
-        { source: '/encuentros/proximo', destination: '/api/next-encuentros-codear' },
-        { source: '/anuario/2020', destination: '/yearbook/2020' },
-      ];
-    },
-    catchAllRouting: true,
+  async rewrites() {
+    return [
+      { source: '/', destination: '/' },
+      { source: '/conocenos', destination: '/about' },
+      { source: '/codigo-de-conducta', destination: '/coc' },
+      { source: '/comunicaciones', destination: '/comuniques' },
+      { source: '/comunidades', destination: '/communities' },
+      { source: '/conocimiento', destination: '/knowledge' },
+      { source: '/encuentros/proximo', destination: '/api/next-encuentros-codear' },
+      { source: '/anuario/2020', destination: '/yearbook/2020' },
+    ];
   },
+  catchAllRouting: true,
 };
